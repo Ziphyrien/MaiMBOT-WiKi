292d2310ddbc151a16a87f4437e9392d8f6c9718
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-08 00:02:25 +0800

    Add files via upload

---
+++ .vuepress/navbar.ts
@@ -0,0 +1,9 @@
+import { defineNavbarConfig } from 'vuepress-theme-plume'
+
+export const navbar = defineNavbarConfig([
+  { text: '首页', link: '/' },
+  {
+    text: '指南',
+    items: [{ text: '部署', link: '/notes/Windows部署教程.md' }]
+  },
+])
