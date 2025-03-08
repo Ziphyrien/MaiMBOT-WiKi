1b3428a2c5f2e907a8fbdce602765c3d0d8fd6ed
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-09 01:21:39 +0800

    Update notes.ts

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -2,13 +2,13 @@ import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 
 const root = defineNoteConfig({
   dir: '/',
-  link: '/docs/',
+  link: 'docs/',
   sidebar: 'auto',
 })
 
 const deploy = defineNoteConfig({
   dir: '/部署教程',
-  link: '/docs/deploy/',
+  link: 'docs/deploy/',
   sidebar: 'auto',
 })
 
