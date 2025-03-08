446e5273e3e99c46cca6c81160f1ef2ef61cf6f6
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-08 23:26:17 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,8 +2,4 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
-  {
-    text: '指南',
-    items: [{ text: '部署', link: '/notes/1.' }]
-  },
 ])
