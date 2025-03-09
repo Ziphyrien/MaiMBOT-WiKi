cdd3268e74d3ee2941d9731a3b170822c84ef991
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-09 11:58:02 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -13,8 +13,9 @@ export default defineThemeConfig({
   social: [
     { icon: 'github', link: 'https://github.com/SengokuCola/MaiMBot/' },
     { icon: 'qq', link: 'https://qm.qq.com/q/kdJox0XMXK' },
+    { icon: 'clarity--email-outline-alerted', link: 'https://qm.qq.com/q/kdJox0XMXK' }
   ],
-  navbarSocialInclude: ['github','qq'], // 允许显示在导航栏的 social 社交链接
+  navbarSocialInclude: ['github','qq','clarity--email-outline-alerted'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
