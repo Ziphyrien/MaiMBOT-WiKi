01852db2467d638e3a17128b0b1a7487cad1941f
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-09 09:22:37 +0800

    Update notes.ts

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -1,31 +1,13 @@
-// .vuepress/notes.ts
 import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 
-// 项目介绍配置
-const projectIntro = defineNoteConfig({
-  dir: '',  // 直接在notes目录下
+const root = defineNoteConfig({
+  dir: '',
   link: '/docs/',
-  sidebar: [
-    '1.项目介绍.md'  // 直接引用根目录下的文件
-  ]
+  sidebar: 'auto',
 })
 
-// 部署教程配置
-const deploymentTutorial = defineNoteConfig({
-  dir: '部署教程',
-  link: '/deploy/',
-  sidebar: [
-    'README.md',    // 部署前言
-    '准备工作.md',
-    // 如果有其他部署相关文档可继续添加
-  ]
-})
-
-export default defineNotesConfig({
+export const notes = defineNotesConfig({
   dir: 'notes',
   link: '/',
-  notes: [
-    projectIntro,
-    deploymentTutorial,
-  ],
+  notes: [root]
 })
