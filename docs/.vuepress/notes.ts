809f26d809bbf3724f9e56a25dcd41a10a0e2cae
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-09 11:47:19 +0800

    Update notes.ts

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -6,9 +6,10 @@ const root = defineNoteConfig({
   sidebar: [
     '项目介绍',
     {
-      text: '开始部署',
+      text: '开始部署',      
+      link: '部署教程/README',
       items: [
-        '部署教程/准备工作'
+        '部署教程/准备工作'    
       ],  
     },
   ],  
