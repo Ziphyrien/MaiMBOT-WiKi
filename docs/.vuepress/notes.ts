8252e40975b9264893427c8fb942ea5d7f0b0d8b
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-09 10:43:46 +0800

    Update notes.ts

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -3,18 +3,18 @@ import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 const root = defineNoteConfig({
   dir: '',
   link: '/docs/',
-  sidebar: {
-    ['项目介绍.md'],
+  sidebar: [
+    '项目介绍.md', // 修正为字符串格式
     {
       dir: '部署教程',
       link: '/deploy/',
       sidebar: 'auto',
     },
-  },  
+  ],  
 })
 
 export const notes = defineNotesConfig({
   dir: 'notes',
   link: '/',
-  notes: [root,deploy],
+  notes: [root], 
 })
