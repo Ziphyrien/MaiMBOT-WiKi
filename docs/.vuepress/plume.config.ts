8cae8f64d096ad7c44bef5ea70224f409188cc40
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-03-12 22:04:43 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -31,7 +31,7 @@ export default defineThemeConfig({
 
   /* 站点页脚 */
   footer: {
-    message: 'Bot by <a target="_blank" href="https://space.bilibili.com/9116293">千石可乐</a> ＆ WiKi by <a target="_blank" href="https://space.bilibili.com/517481506?spm_id_from=333.1007.0.0">Ziphyrien ©2025</a>',
+    message: 'Copyright © 2025-present <a target="_blank" href="https://space.bilibili.com/517481506?spm_id_from=333.1007.0.0">Ziphyrien ©2025</a>',
   //   copyright: '',    
   },
 
