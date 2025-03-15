933da93135d0793891a3a950237cdff6719fa7d9
Ziphyrien <Hanson132567@outlook.com>
2025-03-16 00:35:14 +0800

    modified:   docs/.vuepress/config.ts 	modified:   package.json

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,6 +8,10 @@ export default defineUserConfig({
   title: 'MaiMBOT 8aka WiKi',
   description: '',
 
+  module.exports = {
+    plugins: ['cursor-effects'],
+  }  
+
   head: [
     // 配置站点图标
     ['link', { rel: 'icon', type: 'image/png', href: '/MaiM.png' }],
@@ -21,7 +25,7 @@ export default defineUserConfig({
   ],
   bundler: viteBundler(),
   shouldPrefetch: false, // 站点较大，页面数量较多时，不建议启用
-  
+
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
     hostname: 'https://maimbot.pages.dev',
